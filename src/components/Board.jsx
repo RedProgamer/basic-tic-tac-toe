7be764ecb4413ace7b3f535137dfd1904a9303b6
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2021-12-04 16:26:49 +0530

    Added time machine feature

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,48 +1,15 @@
 import React from "react";
-import { useState } from "react/cjs/react.development";
 import Square from "./Square";
-import calculateWinner from "./Logic";
 import './styles.css';
 
 
-function Board() {
-
-    const [State, setState] = useState({
-        squares: Array(9).fill(null),
-        isNext: true,
-    });
-
-    console.log(State.squares);
-
-    const handleClicker = i => {
-        const new_squares = State.squares.slice();
-        
-        if(calculateWinner(new_squares) || new_squares[i]) {
-            return;
-        }
-
-        new_squares[i] = State.isNext ? 'X' : 'O';
-        setState({squares: new_squares, isNext: !State.isNext});
-    }
-
+function Board(props) {
     const renderSquare = (i) => {
-      return <Square value={State.squares[i]} onClick={() => { handleClicker(i) }}/>;
-    }
-
-    const winner = calculateWinner(State.squares);
-    let status;
-
-    if (winner) {
-        status = 'Winner: ' + winner;
-    }else if(State.squares.includes(null)) {
-        status = 'Next player: ' + (State.isNext ? 'X' : 'O');
-    }else {
-        status = 'Draw!';
+      return <Square value={props.squares[i]} onClick={() => { props.onClick(i) }}/>;
     }
     
     return (
         <div className="wrapper">
-            <div className="status"><span className='current-player'>{status}</span></div>
             <div className="grid">
                 <div className="board-row">
                     {renderSquare(0)}
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,11 +1,58 @@
 import { useState } from 'react';
+import Board from './Board';
+import calculateWinner from './Logic';
+import './styles.css';
 
 function Game() {
-    
-    const [history, setHistory] = useState([{}])
-    
-    return (
+    const [history, setHistory] = useState({
+        history: [{
+            squares: Array(9).fill(null)
+        }],
+        isNext: true,
+    });
 
+    const histories = history.history;
+    const current = histories[history.history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+
+    if(winner) {
+        status = 'Winner : ' + winner;
+    }else {
+        status = 'Next Player : ' + (history.isNext ? 'X' : 'O');
+    }
+
+    function handleClick(i) {
+        const histories = history.history;
+        const current = histories[history.history.length - 1];
+        const squares = current.squares.slice();
+
+        console.log(squares);
+        
+        if (calculateWinner(squares) || squares[i]) {
+          return;
+        }
+        
+        squares[i] = history.isNext ? 'X' : 'O';
+        
+        setHistory({
+          history: histories.concat([{
+            squares: squares,
+          }]),
+          isNext: !history.isNext,
+        });
+    }
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board squares={current.squares} onClick={(i) => { handleClick(i) }} />
+            </div>
+            <div className="game-info">
+            <div>{status}</div>
+            <ol>{/* TODO */}</ol>
+            </div>
+      </div>
     );
 }
 
